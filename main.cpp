c5092f71a9bee0b68ed02b4d51206b420a0c8a17
Satoshi Ogata <satosystems@gmail.com>
2023-05-30 01:08:31 +0900

    Implement first version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
+#include <cstdlib>
 #include <iostream>
+#include <sstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <chatgpt.h>
+
+void callback(const chatgpt_callback_kind kind, const char *s) {
+    if (kind == chatgpt_callback_kind_content) {
+        std::cout << s << std::flush;
+    } else if (kind == chatgpt_callback_kind_stop) {
+        std::cout << std::endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    const char *api_key = std::getenv("OPENAI_API_KEY");
+    if (api_key == nullptr) {
+        std::cerr << "OPENAI_API_KEY environment variable not found." << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (argc == 1) {
+        std::cerr << "Command line arguments are not found." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::stringstream ss;
+    ss << argv[1];
+    for (int i = 2; i < argc; ++i) {
+        ss << ' ' << argv[i];
+    }
+    std::string content = ss.str();
+
+    chatgpt_completions(api_key, content.c_str(), callback);
+
+    return EXIT_SUCCESS;
 }
